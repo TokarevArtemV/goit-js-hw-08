8f9e3d92754ab84f7e949984dae642b6561f910b
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-10-22 16:12:52 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import _ from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on(
   'timeupdate',
-  throttle(onPlay, 1000));
+  _.throttle(onPlay, 1000));
   
 function onPlay(evt) {
   localStorage.setItem('videoplayerCurrentTime', JSON.stringify(evt.seconds));
